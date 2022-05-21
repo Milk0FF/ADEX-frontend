80d9e3e0e36bb550c181df10189f110d1a9f6c3e
Milk0FF <ivan.milko@yandex.ru>
2022-05-22 01:30:30 +0600

    Start implementation personal profile page

--- src/views/PersonalProfilePage.vue
+++ src/views/PersonalProfilePage.vue
@@ -34,7 +34,60 @@
   </div>
   <div class="personal-profile">
     <div class="personal-profile__container container container_column">
-
+      <div class="personal-profile__header">
+        <div class="personal-profile__avatar">
+          <img src="../assets/images/user-logo.png"/>
+          <div class="personal-profile__edit"></div>
+        </div>
+        <div class="personal-profile__name">Иванов Иван</div>
+      </div>
+      <div class="personal-profile__content">
+        <h3 class="personal-profile__title"></h3>
+        <div class="personal-profile__field field">
+          <div class="field__title">Имя</div>
+          <input type="text" placeholder="Имя">
+        </div>
+        <div class="personal-profile__field field">
+          <div class="field__title">Фамилия</div>
+          <input type="text" placeholder="Фамилия">
+        </div>
+        <div class="personal-profile__field field">
+          <div class="field__title">О себе</div>
+          <input type="text" placeholder="О себе">
+        </div>
+        <div class="personal-profile__field field">
+          <div class="field__title">Город</div>
+          <input type="text" placeholder="Город">
+        </div>
+        <div class="personal-profile__field field">
+          <div class="field__title">Страна</div>
+          <input type="text" placeholder="Страна">
+        </div>
+        <div class="personal-profile__field field">
+          <div class="field__title">Дата рождения</div>
+          <input type="text" placeholder="Дата рождения">
+        </div>
+        <div class="personal-profile__field field">
+          <div class="field__title">Категории</div>
+          <input type="text" placeholder="Категории">
+        </div>
+        <div class="personal-profile__categories">
+          <div class="personal-profile__category task-category__red">
+            YOUTUBE
+            <div class="task-category__delete"></div>
+          </div>
+          
+          <div class="personal-profile__category task-category__blue">
+            VK
+            <div class="task-category__delete"></div>
+          </div>
+          
+          <div class="personal-profile__category task-category__purple">
+            INSTAGRAM
+            <div class="task-category__delete"></div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <div class="footer">
