61ed0aefb82a300128ec74b3492be9700e25024b
Milk0FF <ivan.milko@yandex.ru>
2022-06-06 20:28:38 +0600

    Start implementation sync chat

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import axios from 'axios';
+
 import AuthLayout from './layouts/AuthLayout.vue';
 import MainLayout from './layouts/MainLayout.vue';
 import NotFoundLayout from './layouts/NotFoundLayout.vue';
-import axios from 'axios';
+
 
 const app = createApp(App);
 
